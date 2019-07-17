2f6d4f3bbed7e4837364726087607d89af8a6d74
xajayanthi <j.velusamy@xearts.jp>
2019-07-17 18:21:27 +0900

    cssファイルを読み込めるようにする

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,24 @@
 module.exports = {
+    entry:'./src/index.js',
+    output:{
+        path:`${__dirname}/dist`,
+        filename:'main.js'
+    },
     // モード値を production に設定すると最適化された状態で、
     // development に設定するとソースマップ有効でJSファイルが出力される
     mode: "development",
-
+    // JS、json以外の読み込み
+    module:{
+        rules:[
+            {
+                test: /\.css$/,
+                use:[
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
     // ローカル開発用環境を立ち上げる
     // 実行時にブラウザが自動的に localhost を開く
     devServer: {
