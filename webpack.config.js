75f6eedcc911dc4f6396f3093729f8c4b48965d4
xajayanthi <j.velusamy@xearts.jp>
2019-07-17 16:12:01 +0900

    webpack.config.jsを設定

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    // メインとなるJavaScriptファイル（エントリーポイント）
+    entry: `./src/index.js`,
+    // モード値を production に設定すると最適化された状態で、
+    // development に設定するとソースマップ有効でJSファイルが出力される
+    mode: "development",
+    // ファイルの出力設定
+    output: {
+        //  出力ファイルのディレクトリ名
+        path: `${__dirname}/dist`,
+        // 出力ファイル名
+        filename: "main.js"
+    }
+};
